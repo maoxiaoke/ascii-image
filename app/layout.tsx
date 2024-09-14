038b206eed7dccf33e7743a9c3bd1ead8f0afb3b
maoxiaoke <maoxiaoke@outlook.com>
2024-09-14 14:43:20 +0800

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ function CloudflareAnalytics() {
     <Script
       defer
       src='https://static.cloudflareinsights.com/beacon.min.js'
-      data-cf-beacon={`{"token": "4013e57b5e5745d0b0b0507ef5a98a33"}`}
+      data-cf-beacon='{"token": "4013e57b5e5745d0b0b0507ef5a98a33"}'
       strategy="afterInteractive"
     />
   )
